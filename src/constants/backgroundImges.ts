a630adc28f8aa79aba06c3d868eaa0566df5bc8d
Huzaifa Shuaib <huzaifashoaib7@gmail.com>
2024-07-11 10:03:11 +0500

    fix background problem

--- src/constants/backgroundImges.ts
+++ src/constants/backgroundImges.ts
@@ -1,6 +1,6 @@
 export const backImages = {
-  backgroundImg: "url('../../src/assets/images/Background.png')",
-  logo: "url('../assets/images/Logo.png')",
-  weatherBackground: "url('../../src/assets/images/WeatherImg.png')",
+  backgroundImg: "url('/src/assets/images/Background.png')",
+  logo: "url('/assets/images/Logo.png')",
+  weatherBackground: "url('/src/assets/images/WeatherImg.png')",
 };
 
