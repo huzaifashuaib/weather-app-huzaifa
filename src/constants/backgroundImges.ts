f38fe7e3aab25902f7c88faccc37ad495f647b39
Huzaifa Shuaib <huzaifashoaib7@gmail.com>
2024-07-11 09:58:48 +0500

    fix groundImg

--- src/constants/backgroundImges.ts
+++ src/constants/backgroundImges.ts
@@ -1,5 +1,6 @@
 export const backImages = {
-  backgroundImg: "url('./src/assets/images/Background.png')",
+  backgroundImg: "url('../../src/assets/images/Background.png')",
   logo: "url('../assets/images/Logo.png')",
   weatherBackground: "url('../../src/assets/images/WeatherImg.png')",
 };
+
